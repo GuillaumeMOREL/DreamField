25d7c0f825f04fabb4ae9871e045309defac62bf
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-04-21 14:49:36 +0200

    Classement
    Rangement par dossier

--- Inscription.php
+++ Inscription.php
@@ -145,7 +145,7 @@
 </footer>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script src="insverif.js"></script>
+  <script src="javascript/insverif.js"></script>
 </body>
 
 
