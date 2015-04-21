8df7127c45a8c5457181991aaf955fd1699483af
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-04-21 14:38:28 +0200

    modif
    simplification de code

--- Inscription.php
+++ Inscription.php
@@ -103,120 +103,25 @@
   </form>
 </div>
 
-<div class="formulaire-co">
+  <div class="formulaire-co">
 
-    <form method="post" action="traitement.php">
-    <p>
-              <label for="mail" class="champ-co"> Email :</label>
-              <input type="email" name="mail" id="mail" class="champ" size="30" placeholder="Ex :azerty@gmail.com " />
-              <br />
-              <br />
-              <label for="pass" class="champ-co" >Mot de passe :</label>
-              <input type="password" name="pass" id="pass" class="champ" minlength="6" /input>
-              <br />
-              <br />
+      <form method="post" action="traitement.php">
+      <p>
+                <label for="mail" class="champ-co"> Email :</label>
+                <input type="email" name="mail" id="mail" class="champ" size="30" placeholder="Ex :azerty@gmail.com " />
+                <br />
+                <br />
+                <label for="pass" class="champ-co" >Mot de passe :</label>
+                <input type="password" name="pass" id="pass" class="champ" minlength="6" /input>
+                <br />
+                <br />
 
-        <div id="validation-co">
-          <input type="submit" name="valider" value="Connection" />
-        </div>
-    </p>
-    </form>
-
-</div> 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script>
-  $(document).ready(function(){
-    
-    var $prenom = $('#prenom'),
-        $nom = $('#nom'),
-        $adresse = $('#adresse')
-        $ville = $('#ville')
-        $tel = $('#tel')
-        $mdp = $('#pass'),
-        $confirmation = $('#passverif'),
-        $mail = $('#mail'),
-        $envoi = $('#validation-ins'),
-        $reset = $('#rafraichir'),
-        $erreur = $('#erreur'),
-        $champ = $('.champ');
-
-    $champ.keyup(function(){
-      if($(this).val().length < 5){ // si la chaîne de caractères est inférieure à 5
-        $(this).css({ // on rend le champ rouge
-          borderColor : 'red',
-          color : 'red'
-        });
-      }
-      else{
-            $(this).css({ // si tout est bon, on le rend vert
-            borderColor : 'green',
-            color : 'green'
-        });
-      }
-    });
-
-    $tel.keyup(function(){
-      if($(this).val().length < 10){ // si la chaîne de caractères est inférieure à 10
-        $(this).css({ // on rend le champ rouge
-          borderColor : 'red',
-          color : 'red'
-        });
-      }
-      else{
-            $(this).css({ // si tout est bon, on le rend vert
-            borderColor : 'green',
-            color : 'green'
-        });
-      }
-    });  
-
-    $passverif.keyup(function(){
-      if($(this).val() != $mdp.val()){ // si la confirmation est différente du mot de passe
-        $(this).css({ // on rend le champ rouge
-          borderColor : 'red',
-          color : 'red'
-        });
-      }
-      else{
-        $(this).css({ // si tout est bon, on le rend vert
-          borderColor : 'green',
-          color : 'green'
-        });
-      }
-    });
-
-    $envoi.click(function(e){
-      e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
-
-      // puis on lance la fonction de vérification sur tous les champs :
-      verifier($prenom);
-      verifier($nom);
-      verifier($adresse);
-      verifier($ville);
-      verifier($mdp);
-      verifier($confirmation);
-      verifier($mail);
-    });
-
-    $reset.click(function(){
-      $champ.css({ // on remet le style des champs comme on l'avait défini dans le style CSS
-        borderColor : '#ccc',
-        color : '#555'
-      });
-      $erreur.css('display', 'none'); // on prend soin de cacher le message d'erreur
-    });
-
-    function verifier(champ){
-      if(champ.val() == ""){ // si le champ est vide
-        $erreur.css('display', 'block'); // on affiche le message d'erreur
-        champ.css({ // on rend le champ rouge
-          borderColor : 'red',
-          color : 'red'
-        });
-      }
-    }
-  });
-</script> 
+          <div id="validation-co">
+            <input type="submit" name="valider" value="Connection" />
+          </div>
+      </p>
+      </form>
+  </div> 
 </div>
 
 <footer>
@@ -239,7 +144,8 @@
   </div>
 </footer>
 
-   
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="insverif.js"></script>
 </body>
 
 
