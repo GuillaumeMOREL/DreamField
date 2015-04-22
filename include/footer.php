5a97015404509bf71150e2c9f864511a82662912
GuillaumeMOREL <guimorel.gm@gmail.com>
2015-04-22 23:14:09 +0200

    Modif footer
    Stabilisation des différents champs.

--- include/footer.php
+++ include/footer.php
@@ -4,17 +4,21 @@
 </head>
 <div class="footer-container">
   <footer>
-        <div id="texte_footer">
+        <div id="champ-footer">
           <ul>
-            <li><a href="mentions_legales.html"> Mentions legales </a></li>
-            <li><a href="qui_sommes_nous.html"> Qui sommes nous </a></li>
-            <li><a href="nous_contacter.html"> Nous contacter </a></li>
-            <li><a href="plan_du_site.html"> Plan du site </a></li> 
-            <li><a href="charte_de_confidentialite.html"> Charte de confidentialité </a></li>
-            <li><a href="email.html"> <img class="logo_reseaux_sociaux" src="/logo/email.png"/></a></li>
-            <li><a href="facebook"><img class="logo_reseaux_sociaux" src="/logo/facebook.png"/></a></li>
-            <li><a href="twitter"><img class="logo_reseaux_sociaux" src="/logo/twitter.png"/></a></li>
-            <li><a href="google"><img class="logo_reseaux_sociaux" src="/logo/google.png"/></a></li>
+            <div id="text-footer">
+              <li><a href="mentions_legales.html"> Mentions legales </a></li>
+              <li><a href="qui_sommes_nous.html"> Qui sommes nous </a></li>
+              <li><a href="nous_contacter.html"> Nous contacter </a></li>
+              <li><a href="plan_du_site.html"> Plan du site </a></li> 
+              <li><a href="charte_de_confidentialite.html"> Charte de confidentialité </a></li>
+            </div>
+            <div id="logo-footer">
+              <li><a href="email.html"> <img class="logo_reseaux_sociaux" src="/logo/email.png"/></a></li>
+              <li><a href="facebook"><img class="logo_reseaux_sociaux" src="/logo/facebook.png"/></a></li>
+              <li><a href="twitter"><img class="logo_reseaux_sociaux" src="/logo/twitter.png"/></a></li>
+              <li><a href="google"><img class="logo_reseaux_sociaux" src="/logo/google.png"/></a></li>
+            </div>
           </ul>
         </div>
   </footer>
