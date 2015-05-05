335e7253e316ad84009776beb145ff6de25b337f
JeremyPineau <jeremailpineau@gmail.com>
2015-05-05 14:04:04 +0200

    la crise!

---
+++ annoncesfruits.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />   
+<link rel="Stylesheet" href="Style_annoncesfruits.css" />
+<title> DREAMFIELD</title>
+</head>
+  <body>
+  	<!--
+    <header>
+      <?php include("include/header.php"); ?>
+    </header>
+	-->
+
+
+
+    <h2>
+    	Choisissez vos fruits dans la liste proposée:
+    </h2>
+    <p>
+    	Nous vous conseillons en premier lieu de cocher les differents fruits que vous souhaitez acheter
+    	afin de vous creer une liste de course qui vous permettra de ne rien oublier,
+    	et qui nous aidera à choisir le vendeur le plus adapter à votre demande.
+    </p>
+    <section>
+	<a href=""><img src="abricot.jpg" alt="abricot" title="Cliquez ici pour voir les differentes ventes d'abricots" /></a> </br>
+	<h3> ABRICOTS </h3>
+	<input type="checkbox" name="ListeAbricot" id="ListeAbricot" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><a href=""><a href=""><img src="banane.jpg" alt="banane" title="Cliquez ici pour voir les differentes ventes de bananes" /></a> </br>
+	<h3> BANANES </h3>
+	<input type="checkbox" name="ListeBanane" id="ListeBanane" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><a href=""><img src="clementine.jpg" alt="clementine" title="Cliquez ici pour voir les differentes ventes de clementines" /></a> </br>
+	<h3> CLÉMENTINES </h3>
+	<input type="checkbox" name="ListeClementine" id="ListeClementine" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><img src="datte.jpg" alt="datte" title="Cliquez ici pour voir les differentes ventes de dattes" /></a> </br>
+	<h3> DATTES </h3>
+	<input type="checkbox" name="ListeDatte" id="ListeDatte" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><img src="figue.jpg" alt="figue" title="Cliquez ici pour voir les differentes ventes de figues" /></a> </br>
+	<h3> FIGUES </h3>
+	<input type="checkbox" name="ListeFigue" id="ListeFigue" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><img src="grenade.jpg" alt="grenade" title="Cliquez ici pour voir les differentes ventes de grenades" /></a> </br>
+	<h3> GRENADES </h3>
+	<input type="checkbox" name="ListeGrenade" id="ListeGrenade" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><img src="prune.jpg" alt="prune" title="Cliquez ici pour voir les differentes ventes de prunes" /></a> </br>
+	<h3> PRUNES </h3>
+	<input type="checkbox" name="ListePrune" id="ListePrune" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+	<section>
+	<a href=""><img src="kiwi.jpg" alt="kiwi" title="Cliquez ici pour voir les differentes ventes de kiwis" /></a> </br>
+	<h3> KIWIS </h3>
+	<input type="checkbox" name="ListeKiwi" id="ListeKiwi" title="cette liste permet de vous rappeler ce que vous devez acheter"/>
+	Ajouter ce fruit à votre liste de course
+	</section>
+
+
+	<!--
+    <footer>
+      <?php include("include/footer.php"); ?>
+    </footer>  
+	-->
+  </body>
+</html>
