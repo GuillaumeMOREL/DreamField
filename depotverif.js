262549eb19e7d352c771b4e29e7a2b5440a6c35d
JeremyPineau <jeremailpineau@gmail.com>
2015-05-23 15:51:28 +0200

    On m a dit de commit, je commit :/
    Meme si c est pas mis en mvc et que je sais pas vous passer ma bdd

--- depotverif.js
+++ depotverif.js
@@ -4,7 +4,7 @@
     var 
         $date=$('#date');
         $prix=$('#prix');
-
+        $envoi = $('#formulaire-depot'),
 
     $date.keyup(function(){
       if($(this).val().length < 10){ // si la chaîne de caractères est inférieure à 10
@@ -42,5 +42,32 @@
 
 $("#PoidsQuant").change(function() {
     if ( $("#PoidsQuant").val() == "mg" || $("#PoidsQuant").val() == "g" || $("#PoidsQuant").val() == "kg"){ $("#resultat").text("le Kg"); }
-    if ( $("#PoidsQuant").val() == "quantite" ){ $("#resultat").text("l'unité"); }
+    if ( $("#PoidsQuant").val() == "unite" ){ $("#resultat").text("l'unité"); }
 });
+/*
+
+    $envoi.click(function(e){
+      e.preventDefault(); // on annule la fonction par défaut du bouton d'envoi
+
+      // puis on lance la fonction de vérification sur tous les champs :
+      verifier($prenom);
+      verifier($nom);
+      verifier($adresse);
+      verifier($ville);
+      verifier($mdp);
+      verifier($confirmation);
+      verifier($mail);
+    });
+
+
+        function verifier(champ){
+      if(champ.val() == ""){ // si le champ est vide
+        $erreur.css('display', 'block'); // on affiche le message d'erreur
+        champ.css({ // on rend le champ rouge
+          borderColor : 'red',
+          color : 'red'
+        });
+      }
+    }
+  });
+*/
